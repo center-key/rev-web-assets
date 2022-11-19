2ad37d9d532ae2afb2385c2a32fe59cd8f6012a1
dpilafian <dem@centerkey.com>
2022-11-19 02:21:15 -0800

    Use cli-argv-util to parse flags

--- bin/cli.js
+++ bin/cli.js
@@ -20,29 +20,23 @@
 //    $ node bin/cli.js --cd=spec/fixtures source target --meta-content-base=https://example.net
 
 // Imports
+import { cliArgvUtil } from 'cli-argv-util';
 import { revWebAssets } from '../dist/rev-web-assets.js';
-import chalk            from 'chalk';
-import log              from 'fancy-log';
+import chalk from 'chalk';
+import log   from 'fancy-log';
 
-// Parameters
-const validFlags =  ['cd', 'manifest', 'meta-content-base', 'note', 'quiet', 'summary'];
-const args =        process.argv.slice(2);
-const flags =       args.filter(arg => /^--/.test(arg));
-const flagMap =     Object.fromEntries(flags.map(flag => flag.replace(/^--/, '').split('=')));
-const flagOn =      Object.fromEntries(validFlags.map(flag => [flag, flag in flagMap]));
-const invalidFlag = Object.keys(flagMap).find(key => !validFlags.includes(key));
-const params =      args.filter(arg => !/^--/.test(arg));
-
-// Data
-const source = params[0];
-const target = params[1];
+// Parameters and flags
+const validFlags = ['cd', 'manifest', 'meta-content-base', 'note', 'quiet', 'summary'];
+const cli =        cliArgvUtil.parse(validFlags);
+const source =     cli.params[0];
+const target =     cli.params[1];
 
 // Reporting
 const printReport = (results) => {
    const name =      chalk.gray('rev-web-assets');
    const source =    chalk.blue.bold(results.source);
    const target =    chalk.magenta(results.target);
-   const arrow =     { big: chalk.gray.bold('➤➤➤'), little: chalk.gray.bold(' ⟹  ') };  //extra space for alignment
+   const arrow =     { big: chalk.gray.bold(' ⟹  '), little: chalk.gray.bold('→') };
    const infoColor = results.count ? chalk.white : chalk.red.bold;
    const info =      infoColor(`(files: ${results.count}, ${results.duration}ms)`);
    log(name, source, arrow.big, target, info);
@@ -51,24 +45,24 @@ const printReport = (results) => {
       const dest =   chalk.green(detail.destPath.substring(results.target.length + 1));
       log(name, origin, arrow.little, dest);
       };
-   if (!flagOn.summary)
+   if (!cli.flagOn.summary)
       results.manifest.forEach(logDetail);
    };
 
 // Revision Web Assets
 const error =
-   invalidFlag ?       'Invalid flag: ' + invalidFlag :
-   !source ?           'Missing source folder.' :
-   !target ?           'Missing target folder.' :
-   params.length > 2 ? 'Extraneous parameter: ' + params[2] :
+   cli.invalidFlag ?     cli.invalidFlagMsg :
+   !source ?             'Missing source folder.' :
+   !target ?             'Missing target folder.' :
+   cli.paramsCount > 2 ? 'Extraneous parameter: ' + cli.params[2] :
    null;
 if (error)
    throw Error('[rev-web-assets] ' + error);
 const options = {
-   cd:              flagMap.cd ?? null,
-   metaContentBase: flagMap['meta-content-base'] ?? null,
-   saveManifest:    flagOn.manifest,
+   cd:              cli.flagMap.cd ?? null,
+   metaContentBase: cli.flagMap.metaContentBase ?? null,
+   saveManifest:    cli.flagOn.manifest,
    };
 const results = revWebAssets.revision(source, target, options);
-if (!flagOn.quiet)
+if (!cli.flagOn.quiet)
    printReport(results);
